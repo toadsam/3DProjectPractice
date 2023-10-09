a18b28098ddf44f2d5ca553a7cc2fd811a44b0a8
toadsam <75034052+toadsam@users.noreply.github.com>
2023-10-10 04:40:18 +0900

    [ADD] 퀘스트 거의 구현 (-재훈)
    퀘스트 거의 구현

--- Assets/Scripts/Managers/TalkManager.cs
+++ Assets/Scripts/Managers/TalkManager.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,23 +14,44 @@ public class TalkManager : MonoBehaviour
     private void GenerateData()
     {
         talkData.Add(1000, new string[] { "안녕?? 혹시 나 좀 도와줄 수 있어?", "내 친구 좀 구해줘라" });
+        talkData.Add(2000, new string[] { "나는 바보야....ㅠㅠ", "내 친구 좀 구해줘라" });
 
         //Quest Talk
-        talkData.Add(10 + 1000, new string[] {"안녕.......","너가 그 유명한 재훈이니?","내 친구가 잡혀있어....혹시 도와줄 수 있을까?","만약 도와준다면 한강뷰의 집을 너에게 주도록 할게" });
-        talkData.Add(11 + 2000, new string[] { "도와줘서 고마워!","한강 뷰를 너에게 줄게" });
+        talkData.Add(10 + 1000, new string[] { "안녕.......", "너가 그 유명한 재훈이니?", "내 친구가 잡혀있어....혹시 도와줄 수 있을까?", "만약 도와준다면 한강뷰의 집을 너에게 주도록 할게" });
+        talkData.Add(11 + 2000, new string[] { "도와줘서 고마워!", "한강 뷰를 너에게 줄게" });
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 
     public string GetTalk(int id, int talkIndex) //대화 내용을 가져오는 과정
     {
+        if (!talkData.ContainsKey(id)) //딕셔너리에 kety가 존재하는지 검사
+        {
+            if (!talkData.ContainsKey(id - id % 10))
+            {
+
+                if (talkIndex == talkData[id - id % 100].Length)
+                    return null;
+                else
+                    return talkData[id - id % 100][talkIndex];
+            }
+            else
+            {
+                //해당 퀘스트 진행 순서 대사가 없을 때.
+                //퀘스트 맨 처음 대사를 가지고 온다.
+                if (talkIndex == talkData[id - id % 10].Length)
+                    return null;
+                else
+                    return talkData[id - id % 10][talkIndex];
+            }
+        }
         if (talkIndex == talkData[id].Length)
             return null;
         else
-        return talkData[id][talkIndex];
+            return talkData[id][talkIndex];
     }
 }
