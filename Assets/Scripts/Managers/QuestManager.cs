bcabd20da9b969c7a7961b3c24a6d6c3f094cc54
toadsam <75034052+toadsam@users.noreply.github.com>
2023-10-10 01:17:07 +0900

    [ADD] 퀘스트 추가 (-재훈)
    퀘스트 추가

--- Assets/Scripts/Managers/QuestManager.cs
+++ Assets/Scripts/Managers/QuestManager.cs
@@ -1,6 +1,3 @@
-using JetBrains.Annotations;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,7 +16,7 @@ public class QuestManager : MonoBehaviour
 
     private void GenerateData()
     {
-        questList.Add(10, new QuestData("친구를 구해줘!!",new int[] {1000,2000}));
+        questList.Add(10, new QuestData("친구를 구해줘!!", new int[] { 1000, 2000 }));
         questList.Add(20, new QuestData("한강 뷰 보러가기", new int[] { 5000, 2000 }));
     }
 
@@ -30,12 +27,12 @@ public class QuestManager : MonoBehaviour
 
     public void CheckQuest(int id)//대화 진행을 위해 퀘스트 대화순서를 올리는 함수 생성
     {
-       if(id == questList[questId].npcId[questActionIndex])
-        questActionIndex++;
+        if (id == questList[questId].npcId[questActionIndex])
+            questActionIndex++;
 
-        if (questActionIndex == questList[questId].npcId.Length);
-        NextQuest();
-    } 
+        if (questActionIndex == questList[questId].npcId.Length)
+            NextQuest();
+    }
 
     void NextQuest() //다음으로 가는 퀘스트 -> 10늘려주고 인덱스는 다시 0으로한다.
     {
--- Assets/Scripts/Quest/ObgData.cs
+++ Assets/Scripts/Quest/ObgData.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ObgData : MonoBehaviour
 {
     public int id;
     public bool isNpc;
   
+   
 }
